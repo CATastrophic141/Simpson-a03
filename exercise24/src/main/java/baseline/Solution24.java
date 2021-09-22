a984a80054a5b20157b41accc67aaeb7680364f3
Rylan S <rylan.simpson.02@gmail.com>
2021-09-21 23:54:39 -0400

    Completed exercise 24

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -11,32 +11,48 @@ import java.util.Arrays;
 class Anagram{
     Scanner input = new Scanner(System.in);
 
-    /*Prompt user for a string
-    * return user input which is to be saved to a variable*/
-    private String promptAnagramString(int stringNum){
-
+    /*Prompt user for a string, using argument to validate the string identity
+    * save user input to string variable
+    * return string set to uppercase using .toUpperCase
+    *         (Set to uppercase for case insensitivity) */
+    String promptAnagramString(int stringNum){
+        System.out.printf("Please input a string %d%n", stringNum);
+        String userString = input.nextLine();
+        return userString.toUpperCase();
     }
 
     /* Convert string input into a character array using .toCharArray()
     * Sort array using Arrays.sort()
-    * Return sorted character array*/
-    private char[] anagramStringPrep(String[] string){
-
+    * Convert array back to string
+    * Return sorted string*/
+    private String anagramStringPrep(String str){
+        char[] charArray = str.toCharArray();
+        Arrays.sort(charArray);
+        return Arrays.toString(charArray);
     }
 
-    /* Compare two sorted strings
+    /* Call anagramStringPrep for sortedString1, save result to new sortedString1 variable
+    * Call anagramStringPrep for sortedString2, save result to new sortedString2 variable
+    * Compare two sorted strings
     * If the strings are equivalent return true
     * Else return false*/
-    private boolean anagramCompare(char[] sortedString1, char[] sortedString2){
-
+    boolean anagramCompare(String string1, String string2){
+        String sortedString1 = anagramStringPrep(string1);
+        String sortedString2 = anagramStringPrep(string2);
+        return sortedString1.equals(sortedString2);
     }
 
     /*If isAnagram is true
     *       Print that the strings are anagrams
     * Else
     *       Print that the strings are not anagrams*/
-    private void anagramResult(boolean isAnagram){
-
+    void anagramResult(boolean isAnagram){
+        if(isAnagram){
+            System.out.printf("The two strings are anagrams%n");
+        }
+        else{
+            System.out.printf("The two strings are not anagrams%n");
+        }
     }
 }
 
@@ -44,12 +60,14 @@ public class Solution24 {
     public static void main(String[] args){
         Anagram anagram = new Anagram();
 
-        /*Introduce program with print, requesting for the input of two strings
+        /*Print that two strings will be checked to see if they are anagrams
         * Call two iterations of promptAnagramString saved to two separate string Variables
-        * Call two iterations of anagramStringPrep saved to two separate character arrays
         * Call anagramCompare, save result to boolean variable
         * Call anagramResult*/
-
-
+        System.out.printf("Enter two strings to check if they are anagrams%n");
+        String string1 = anagram.promptAnagramString(1);
+        String string2 = anagram.promptAnagramString(2);
+        boolean result = anagram.anagramCompare(string1, string2);
+        anagram.anagramResult(result);
     }
 }
