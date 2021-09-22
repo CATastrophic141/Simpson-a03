7a9b08f31ccfda58458e368834112e248436f465
Rylan S <rylan.simpson.02@gmail.com>
2021-09-21 23:07:22 -0400

    Creation of repository and addition of exercise 24's skeleton and pseudo-comments

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,55 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
+package baseline;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Anagram{
+    Scanner input = new Scanner(System.in);
+
+    /*Prompt user for a string
+    * return user input which is to be saved to a variable*/
+    private String promptAnagramString(int stringNum){
+
+    }
+
+    /* Convert string input into a character array using .toCharArray()
+    * Sort array using Arrays.sort()
+    * Return sorted character array*/
+    private char[] anagramStringPrep(String[] string){
+
+    }
+
+    /* Compare two sorted strings
+    * If the strings are equivalent return true
+    * Else return false*/
+    private boolean anagramCompare(char[] sortedString1, char[] sortedString2){
+
+    }
+
+    /*If isAnagram is true
+    *       Print that the strings are anagrams
+    * Else
+    *       Print that the strings are not anagrams*/
+    private void anagramResult(boolean isAnagram){
+
+    }
+}
+
+public class Solution24 {
+    public static void main(String[] args){
+        Anagram anagram = new Anagram();
+
+        /*Introduce program with print, requesting for the input of two strings
+        * Call two iterations of promptAnagramString saved to two separate string Variables
+        * Call two iterations of anagramStringPrep saved to two separate character arrays
+        * Call anagramCompare, save result to boolean variable
+        * Call anagramResult*/
+
+
+    }
+}
