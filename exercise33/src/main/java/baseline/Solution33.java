bcd8314e0ec7c2ca330a03045771dd28d1e729a5
Rylan S <rylan.simpson.02@gmail.com>
2021-10-01 21:49:11 -0400

    Created skeleton and pseudocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,66 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
+
+package baseline;
+
+import java.util.Scanner;
+
+class EightBall {
+
+    //Create string array of possible responses
+    String[] responses = {"Yes", "No", "Maybe", "Ask again later"};
+
+    void promptQuestion() {
+        Scanner input = new Scanner(System.in);
+        //Prompt user for question
+        System.out.printf("Please enter your question%n");
+        //Let user input. Scanning not required
+        input.nextLine();
+    }
+
+    int generateRandNum() {
+        //Return random number between 0 and 4000
+            //Use 4000 and upper bound and then add 1 to get a rand num between 1 and 4000
+        return;
+    }
+
+    String selectResponse(int randNum){
+            //If random number is between 1 and 1000
+            if (){
+                //Return "Yes" / Array string 0
+                return;
+            }
+            //If random number is between 1001 and 2000
+            else if (){
+                //Return "No" / Array string 1
+                return;
+            }
+            //If random number is between 2001 and 3000
+            else if () {
+                //Return "Maybe" / Array string 2
+                return;
+            }
+            else () {
+            //Return "Ask again later" / Array string 3
+                return;
+            }
+        }
+    }
+
+
+public class Solution33 {
+    public static void main (String[] args) {
+        EightBall ball = new EightBall();
+        //Prompt user question
+        ball.promptQuestion();
+        //Generate random number
+        ball.generateRandNum();
+        //Generate response using random number
+        ball.selectResponse();
+        //Print out response
+        System.out.printf("%s%n",);
+    }
+}
