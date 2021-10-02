1116bd0ddb22dcbac7a47842ab1e2ec124c757ff
Rylan S <rylan.simpson.02@gmail.com>
2021-10-02 14:47:11 -0400

    Created skeleton and pseudocode for exercise 35

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -3,17 +3,6 @@
  *  Copyright 2021 Rylan Simpson
  */
 
-/*Sometimes you have to locate and remove an entry from a list based on some criteria.
- You may have a deck of cards and need to discard one so it’s no longer in play,
-  or you may need to remove values from a list of valid inputs once they’ve been used.
-   Storing the values in an array makes this process easier. Depending on your language,
-    you may find it safer and more efficient to create a new array instead of modifying the existing one.
-
-        Create a small program that contains a list of employee names.
-         Print out the list of names when the program runs the first time.
-          Prompt for an employee name and remove that specific name from the list of names.
-           Display the remaining employees, each on its own line.*/
-
 package baseline;
 
 import java.util.Arrays;
---
+++ exercise35/src/main/java/Solution35.java
@@ -0,0 +1,52 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Lottery {
+
+    //Generate arraylist to store the names of the candidates
+    ArrayList<String> names = new ArrayList<>();
+
+    ArrayList<String> makeListOfWinners() {
+        Scanner input = new Scanner(System.in);
+        //While loop
+        while() {
+            //If user input is not blank
+            if(){
+                //Add user input to arraylist
+            }
+            else {
+                //Return array list
+                return;
+            }
+        }
+    }
+
+    int generateWinnerIndex(ArrayList<String> nameList) {
+        //Generate random number between 0 and arraylist size-1
+        return;
+    }
+
+    void printWinner(ArrayList<String> nameList, int index) {
+        //Print element of arraylist at randomly chosen index
+        System.out.printf("%n");
+    }
+}
+
+public class Solution35 {
+    public static void main(String[] args) {
+        Lottery raffle = new Lottery();
+        //Print functionality of the program
+        System.out.printf("%n");
+        //Call arraylist initialization method
+        ArrayList<String> = raffle.makeListOfWinners();
+        //Call random number generation
+        int winnerIndex = raffle.generateWinnerIndex();
+        //Call winner printing method
+        raffle.printWinner();
+    }
+}
