2733b1f39738e8175f5796ff9b1ec912afc0a0cd
Rylan S <rylan.simpson.02@gmail.com>
2021-10-03 10:54:26 -0400

    Created skeleton and pseudocode for exercise 38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,67 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Rylan Simpson
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+class ArrayFilter {
+
+    int[] generateIntArrayFromString(String[] numString) {
+        //Take in a string array of integers that has been set up to be separated by " " (numbers.split(" ") in the main method)
+        //Create an integer array for the size of the string array of numbers
+        int[] intArray;
+        //For the length of the string array
+        for (){
+            //Parse the number in the string array into an int, saved into the in array
+            intArray[];
+        }
+        //Return int array
+        return;
+    }
+
+    int[] filterEvenNumbers(int[] intArray) {
+        //Create new int array to save even values
+        int[] evenValues;
+        //for the size of the int array
+        for() {
+            //Initialize variable to even array index
+            int j;
+            //If the value in the int array is divisible by 2
+            if(){
+                //Save value into next available element in the even value array
+                evenValues[];
+                //Increment value of even array index
+            }
+        }
+        //return even value array;
+        return;
+    }
+
+    void printArray(int[] intArray){
+        //for the length of the int array
+        for() {
+            //Print the value at the given index
+            System.out.printf("%n");
+        }
+    }
+}
+
+public class Solution38 {
+    public static void main(String[] args){
+        ArrayFilter filter = new ArrayFilter();
+        Scanner input = new Scanner(System.in);
+        //Prompt user for numbers, separated by spaces
+        System.out.printf("%n");
+        //Save input to string array, with each number string separated by " "
+        String[] listOfNums;
+        //Call int array generation method
+        int[] numberArray;
+        //Call even value filter
+        int[] evenNums;
+        //Call array printing method;
+
+    }
+}
